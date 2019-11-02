64e91822b7c7b9fa3a582a12b24030a5c7d8d5f0
jahan2805 <jahan2805@gmail.com>
2019-11-03 00:18:34 +0100

    added corrections to adap-hw02

--- src/main/java/org/wahlzeit/model/Coordinate.java
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -1,9 +1,25 @@
 package org.wahlzeit.model;
 
+import java.util.Objects;
+
 public class Coordinate {
     private double x;
     private double y;
     private double z;
+
+    @Override
+    public int hashCode() {	final int prime = 31;
+        int result = 1;
+        long temp;
+        temp = Double.doubleToLongBits(x);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(y);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(z);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
     public Coordinate(double x, double y, double z){
         this.x = x;
         this.y = y;
@@ -18,10 +34,13 @@ public class Coordinate {
     public double getZ(){
         return z;
     }
-    protected double getDistance(Coordinate coordinate){
-
-        double distance =  Math.sqrt( Math.pow(this.getX() - coordinate.getX(), 2) + Math.pow( this.getY() - coordinate.getY(), 2) + Math.pow(this.getZ() - coordinate.getZ(), 2));
-        return distance;
+    protected double getDistance(Coordinate coordinate) {
+        if (coordinate == null) {
+            throw new IllegalArgumentException("Coordinate should have a value!");
+        } else {
+            double distance = Math.sqrt(Math.pow(this.getX() - coordinate.getX(), 2) + Math.pow(this.getY() - coordinate.getY(), 2) + Math.pow(this.getZ() - coordinate.getZ(), 2));
+            return distance;
+        }
     }
 
     @Override
@@ -33,11 +52,20 @@ public class Coordinate {
             return isEqual(c);
         }
     }
-    protected boolean isEqual(Coordinate coordinate){
-        if (coordinate.getX() == this.x && coordinate.getY() == this.y && coordinate.getZ() == this.z){
-            return true;
-        }else {
+    private static final double epsilon = 1e-4;
+    protected boolean isEqual(Coordinate coordinate) {
+        if (coordinate == null) {
             return false;
+        } else {
+            if (Double.isNaN(this.x) || Double.isNaN(coordinate.getX()) || Double.isNaN(this.y) || Double.isNaN(coordinate.getY()) || Double.isNaN(this.z) || Double.isNaN(coordinate.getZ())) {
+                return false;
+            } else {
+                boolean eqX = Math.abs(this.getX() - coordinate.getX()) < epsilon;
+                boolean eqY = Math.abs(this.getY() - coordinate.getY()) < epsilon;
+                boolean eqZ = Math.abs(this.getZ() - coordinate.getZ()) < epsilon;
+                return eqX && eqY && eqZ;
+
+            }
         }
     }
 }
--- src/test/java/org/wahlzeit/model/CoordinateTest.java
+++ src/test/java/org/wahlzeit/model/CoordinateTest.java
@@ -7,9 +7,10 @@ import static org.junit.Assert.*;
 
 public class CoordinateTest {
     double epsilon =1e-4;
+    Coordinate isNull;
+    Coordinate c1 = new Coordinate(1.1,2.5,5.5);
     @Test
     public void testGetDistance__equalC() {
-        Coordinate c1 = new Coordinate(1.1, 2.5, 5.5);
         Coordinate c2 = new Coordinate(1.1, 2.5, 5.5);
         double actual = c1.getDistance(c2);
         Assert.assertEquals("Wrong", 0, actual, epsilon);
@@ -17,7 +18,6 @@ public class CoordinateTest {
 
     @Test
     public void testGetDistance__not_equalC() {
-        Coordinate c1 = new Coordinate(1.1, 2.5, 5.5);
         Coordinate c2 = new Coordinate(2.9, 3.9, 9.9);
         double actual = c1.getDistance(c2);
         Assert.assertEquals("Wrong", 4.9558, actual, epsilon);
@@ -25,37 +25,45 @@ public class CoordinateTest {
 
     @Test
     public void testIsEqual(){
-        Coordinate c1 = new Coordinate(1.1,3,5);
-        Coordinate c2 = new Coordinate(1.1,3,5);
+        Coordinate c2 = new Coordinate(1.1,2.5,5.5);
         Coordinate c3 = new Coordinate(2.2,3,5);
         Coordinate c4 = new Coordinate(1.1,3.3,5);
         Coordinate c5 = new Coordinate(1.1,3,5.5);
-        assertTrue(c1.equals(c2));
-        assertFalse(c1.equals(c3));
-        assertFalse(c1.equals(c4));
-        assertFalse(c1.equals(c5));
+        Coordinate c6 = new Coordinate(5.5, Double.NaN, 2);
+        Coordinate c7 = new Coordinate(Double.NaN, 1,2);
+        Coordinate c8 = new Coordinate(4.2, 5.5, Double.NaN);
+        assertTrue(c1.isEqual(c2));
+        assertFalse(c1.isEqual(c3));
+        assertFalse(c1.isEqual(c4));
+        assertFalse(c1.isEqual(c5));
+        assertFalse(c6.isEqual(c1));
+        assertFalse(c7.isEqual(c2));
+        assertFalse(c8.isEqual(c3));
+        assertFalse(c1.isEqual(isNull));
     }
     @Test
     public void testEquals__diffObj(){
-        Coordinate c1 = new Coordinate(1.1,3,5);
         Object o = "abc";
         assertFalse(c1.equals(o));}
 
     @Test
     public void testEquals__someObj(){
-        Coordinate c1 = new Coordinate(1.1,3,5);
         Object o1 = new Object();
-        assertFalse(c1.equals(o1));}
+        assertFalse(c1.equals(o1));
+        assertFalse(c1.hashCode() == o1.hashCode());}
     @Test
     public void testEquals__Same(){
-        Coordinate c1 = new Coordinate(1.1,3,5);
-        Coordinate c2 = new Coordinate(1.1,3,5);
-        assertTrue(c1.equals(c2));}
+        Coordinate c2 = new Coordinate(1.1,2.5,5.5);
+        assertTrue(c1.equals(c2));
+        assertTrue(c1.hashCode() == c2.hashCode());}
     @Test
     public void testEquals__Diff(){
-        Coordinate c1 = new Coordinate(1.1,3,5);
         Coordinate c3 = new Coordinate(2.2,3,5);
         assertFalse(c1.equals(c3));
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void constructorShouldThrowException() {
+    c1.getDistance(isNull);
     }
 
 }
